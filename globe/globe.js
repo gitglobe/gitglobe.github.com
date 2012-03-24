d5828c222ac51ef9534e58ad11de8d5602688ed8
Zhan Shi <coredumpling@gmail.com>
2012-03-23 23:47:53 -0700

    Filter possible duplicates from incoming data by timestamp

--- globe/globe.js
+++ globe/globe.js
@@ -185,7 +185,6 @@ DAT.Globe = function(container, colorFn) {
     opts.animated = opts.animated || false;
     this.is_animated = opts.animated;
     opts.format = opts.format || 'magnitude'; // other option is 'legend'
-    console.log(opts.format);
     if (opts.format === 'magnitude') {
       step = 3;
       colorFnWrapper = function(data, i) { return colorFn(data[i+2]); }
